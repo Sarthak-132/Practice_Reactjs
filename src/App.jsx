718fc5dd11c9f3907b6cabff4a98c8ec6b3bbef3
Sarthak dhoke <sarthakdhoke132@gmail.com>
2024-12-13 22:11:00 +0530

    all started react hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const age = 23;
+
+  const [a , setA] = useState(10)
+
+  const change = () =>{
+
+    setA(20)
+
+  }
+
+
+  const [num, setNum] = useState(0);
+  
+
+  // num = readable element , setNum = writable element
+
   return (
     <div>
-      
+      <h1>Age is : {age}</h1>
+      <h1>value of a is : {a}</h1>
+      <h1>value of num is : {num}</h1>
+      <button onClick={change}>Change</button>
+      <button onClick={()=>setNum(num+10)}>increment</button>
+      <button onClick={()=>setNum(num-10)}>decrement</button>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+// main parent file
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import "./index.css"
